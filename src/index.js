5144e76174049464d07369ef52c4dd1ebf50a034
sharicodes <srosenb@yahoo.com>
2019-02-14 17:50:13 -0500

    finished again practice

--- src/index.js
+++ src/index.js
@@ -1,53 +1,51 @@
-let allDogs = []
+
 document.addEventListener('DOMContentLoaded', () => {
-const dogForm = document.querySelector("#dog-form")
-const dogContainer = document.querySelector('#table-body')
-//console.log(dogContainer)
-const editForm = document.querySelector('#dog-form')
-//console.log(editForm)
-fetch("http://localhost:3000/dogs")
-  .then( response => response.json())
-  .then(dogData => {
-    allDogs = dogData
-    //take my dogs array to make HTML with them in order to add them to DOM
-   let dogsHTML = dogData.map(function(dog){
-     return  `
-    <tr><td class="tblName">${dog.name}  </td> <td class="tblBreed">${dog.breed}</td> <td class="tblSex">${dog.sex}</td> <td><button data-id="${dog.id}" data-action="edit">Edit</button></td></tr>
+const dogTable = document.querySelector("#table-body")
+const editDog = document.querySelector("#dog-form")
+const dogsURL = "http://localhost:3000/dogs"
 
+fetch(dogsURL)
+.then(r => r.json())
+.then(dogData => {
+  let dogHTML = dogData.map(dog =>{
+    return `
+    <tr><td>${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button class="edit" data-id=${dog.id}>Edit</button></td></tr>
     `
-    })
-    dogContainer.innerHTML = dogsHTML.join('')
-  })//end fetch all dogs
+  })//end map
+  dogTable.innerHTML = dogHTML.join("")
+  })//end then
 
-  dogContainer.addEventListener("click", (e)=>{
-  //console.log(e.target)
-  let currDog = allDogs.find(dog => {
-  return dog.id === parseInt(e.target.dataset.id)
-  })
+dogTable.addEventListener("click", (e)=>{
+  //console.log(e.target);
+  if(e.target.className === "edit"){
+    // console.log(e.target.dataset.id)
+    // console.log(editDog)
+    //console.log(editDog.children[4])
+    let targetId = e.target.dataset.id
+    let submitButton = editDog.children[4]
+    submitButton.id = targetId
+    let targetRow = e.target.parentElement.parentElement
+    // console.log(targetRow)
+    let targetName = targetRow.children[0].innerHTML
+    let targetBreed = targetRow.children[1].innerHTML
+    let targetSex = targetRow.children[2].innerHTML
+    //console.log((chosenDogId));
+    let editForm = document.getElementById("dog-form")
+    // console.log(editForm)
+    editForm.children[0].value = targetName
+    editForm.children[1].value = targetBreed
+    editForm.children[2].value = targetSex
+  }//end if statement
+})//end addEventListener
 
-  dogForm.name.value = currDog.name
-  dogForm.breed.value = currDog.breed
-  dogForm.sex.value = currDog.sex
-  dogForm.dogId.value  = currDog.id
-  //console.log(dogForm)
-
-
-
-})//end dog container
-
-dogForm.addEventListener("submit", (e) =>{
-    e.preventDefault()
-
-
-const newName = e.target.name.value
-const newBreed = e.target.breed.value
-const newSex = e.target.sex.value
-const dogId = e.target.dogId.value
-//console.log(dogId)
-
-
-fetch(`http://localhost:3000/dogs/${dogId}`,{
-
+editDog.addEventListener("submit", (e)=>{
+e.preventDefault()
+  let newName = e.target.name.value
+  let newBreed = e.target.breed.value
+  let newSex = e.target.sex.value
+  let dogId = e.target.children[4].id
+  //console.log(newName, newBreed, newSex)
+  fetch(`http://localhost:3000/dogs/${dogId}`,{
   method: "PATCH",
   headers: {
           "Content-Type": "application/json",
@@ -59,27 +57,21 @@ fetch(`http://localhost:3000/dogs/${dogId}`,{
           sex: newSex
         })
   })
-  .then(res => res.json())
-  .then(dogData => {
-    fetch("http://localhost:3000/dogs")
-      .then( response => response.json())
+  .then(r => r.json())
+  .then(dogData =>{
+    fetch(dogsURL)
+      .then(r => r.json())
       .then(dogData => {
-        allDogs = dogData
-        //take my dogs array to make HTML with them in order to add them to DOM
-       let dogsHTML = dogData.map(function(dog){
-         return  `
-        <tr><td class="tblName">${dog.name}  </td> <td class="tblBreed">${dog.breed}</td> <td class="tblSex">${dog.sex}</td> <td><button data-id="${dog.id}" data-action="edit">Edit</button></td></tr>
-
+      let dogHTML = dogData.map(dog =>{
+        return `
+        <tr><td>${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button class="edit" data-id=${dog.id}>Edit</button></td></tr>
         `
-        })
-        dogContainer.innerHTML = dogsHTML.join('')
-      })//end fetch all dogs
+    })//end map
+    dogTable.innerHTML = dogHTML.join("")
+    })//end then
+    editDog.reset()
   })
-})
 
+})//end addEventListener
 
-
-
-})
-
-//end Domcontent loaded
+})//end Domcontent loaded
